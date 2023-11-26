de61841a24cbd164ea16753193cb1505e771dfdf
WSH032 <614337162@qq.com>
2023-11-27 01:52:37 +0800

    build: use `hatch-regex-commit` to auto create git tag

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,6 @@
-// https://commitlint.js.org/#/reference-configuration?id=shareable-configuration
+// refer to: https://commitlint.js.org/#/reference-configuration?id=shareable-configuration
 // Rule: https://karma-runner.github.io/6.4/dev/git-commit-msg.html
-module.exports = { extends: ['@commitlint/config-angular'] };
+// Rule: https://github.com/conventional-changelog/commitlint/tree/master/@commitlint/config-conventional
+
+// NOTE: the extends must consistent with `.pre-commit-config.yaml`
+module.exports = { extends: ['@commitlint/config-conventional'] };
