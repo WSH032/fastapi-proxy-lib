ebab5ca7cb7f0e84d498083a2e27737c87b57ab4
Pavel Serikov <pavel.p.serikov@gmail.com>
2025-01-15 04:14:09 +0300

    docs: add example of `Modify (redefine) response only to particular endpoint` (#37)
    * Add example of redefining particular endpoint
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * docs(CHANGELOG): update `CHANGELOG.md`
    
    * docs: move inline code snippets in the documentation to separate files
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: WSH032 <614337162@qq.com>
    Co-authored-by: Sean Wang <126865849+WSH032@users.noreply.github.com>

---
+++ docs_src/advanced/modify-request.py
@@ -0,0 +1,24 @@
+from collections.abc import Generator
+from typing import Any
+
+import httpx
+from fastapi_proxy_lib.fastapi.app import reverse_http_app
+from httpx import Request
+
+
+class MyCustomAuth(httpx.Auth):
+    # ref: https://www.python-httpx.org/advanced/#customizing-authentication
+
+    def __init__(self, token: str):
+        self.token = token
+
+    def auth_flow(self, request: httpx.Request) -> Generator[Request, Any, None]:
+        # Send the request, with a custom `X-Authentication` header.
+        request.headers["X-Authentication"] = self.token
+        yield request
+
+
+app = reverse_http_app(
+    client=httpx.AsyncClient(auth=MyCustomAuth("bearer_token")),
+    base_url="http://www.httpbin.org/",
+)
---
+++ docs_src/advanced/modify-response.py
@@ -0,0 +1,47 @@
+from collections.abc import AsyncIterator
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from fastapi_proxy_lib.core.http import ReverseHttpProxy
+from starlette.requests import Request
+from starlette.responses import AsyncContentStream, StreamingResponse
+
+proxy = ReverseHttpProxy(base_url="http://www.example.com/")
+
+
+@asynccontextmanager
+async def close_proxy_event(_: FastAPI) -> AsyncIterator[None]:
+    """Close proxy."""
+    yield
+    await proxy.aclose()
+
+
+app = FastAPI(lifespan=close_proxy_event)
+
+
+async def new_content(origin_content: AsyncContentStream) -> AsyncContentStream:
+    """Fake content processing."""
+    async for chunk in origin_content:
+        # do some processing with chunk, e.g transcoding,
+        # here we just print and return it as an example.
+        print(chunk)
+        yield chunk
+
+
+@app.get("/{path:path}")
+async def _(request: Request, path: str = ""):
+    proxy_response = await proxy.proxy(request=request, path=path)
+
+    if isinstance(proxy_response, StreamingResponse):
+        # get the origin content stream
+        old_content = proxy_response.body_iterator
+
+        new_resp = StreamingResponse(
+            content=new_content(old_content),
+            status_code=proxy_response.status_code,
+            headers=proxy_response.headers,
+            media_type=proxy_response.media_type,
+        )
+        return new_resp
+
+    return proxy_response
